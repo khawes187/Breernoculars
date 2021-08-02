449e6f7d407364b5e95f030897ae3199989527ba
Kevin Hawes <khawes187@gmail.com>
2021-08-02 17:45:35 -0400

    Put in the DAO for the address and breweries and models for all, couldnt test by sql queries because I couldn't get the server to run. Will have to test laters since I used subqueries to get the foreign keys.

---
+++ API/Capstone/DAO/BreweryAddressSqlDAO.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+using Capstone.DAO;
+using System.Data.SqlClient;
+
+namespace Capstone.DAO
+{
+    public class BreweryAddressSqlDAO : IBreweryAddressDAO
+    {
+        private readonly string connectionString;
+
+        public BreweryAddressSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public BreweryAddress GetAddress(int addressId)
+        {
+            BreweryAddress returnBreweryAddress = new BreweryAddress();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT addressId, streetAddress, city, stateOrTerritoy, county, postalCode FROM BreweryAddress WHERE addressId = @addressId", conn);
+                    cmd.Parameters.AddWithValue("@addressId", addressId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        returnBreweryAddress = GetBreweryAddressFromReader(reader);
+                        
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return returnBreweryAddress;
+        }
+
+        private BreweryAddress GetBreweryAddressFromReader(SqlDataReader reader)
+        {
+            BreweryAddress breweryAddress = new BreweryAddress();
+            breweryAddress.AddressId = Convert.ToInt32(reader["breweryId"]);
+            breweryAddress.StreetAddress = Convert.ToString(reader["breweryName"]);
+            breweryAddress.City = Convert.ToString(reader["addressId"]);
+            breweryAddress.StateOrTerritory = Convert.ToString(reader["phoneNumber"]);
+            breweryAddress.County = Convert.ToString(reader["website"]);
+            breweryAddress.PostalCode = Convert.ToString(reader["history"]);
+
+            return breweryAddress;
+        }
+    }
+}
