fde0404dd4bd3cc68825b8fd7f7bf47f92bd02dd
Laura Brenner <laura.brenner@ymail.com>
2021-08-05 15:27:06 -0400

    register page

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -33,6 +33,7 @@
         v-model="user.confirmPassword"
         required
       />
+      <div>
       <input
         type="radio"
         id="roleUser"
@@ -53,10 +54,17 @@
         required
       />
       <label class="brewer" for="roleBrewer">Brewer</label>
-      <router-link :to="{ name: 'login' }" tag="button">Have an account?</router-link>
+      </div>
+      <div>
       <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
+        <img src="@/images/editedbutton.gif" width="55" height="65" alt="sumbit" />
+          <div class="createAccount">Create Account</div>
       </button>
+<!--       <router-link :to="{ name: 'login' }" tag="button">
+        <img src="@/images/editedbutton.gif" width="55" height="65" alt="return to login" />
+          <div class="ReturnLogin">Return to Login</div></router-link> -->
+      
+      </div>
     </form>
   </div>
 </body>
@@ -130,8 +138,8 @@ export default {
   padding: 0;
 }
 #register.text-center {
-  margin-left: -60px;
-  margin-right: 60px;
+  margin-left: -40px;
+  margin-right: 40px;
   display: grid;
   position: relative;
   top: 50%;
@@ -154,7 +162,7 @@ h1 {
   font-family: 'Lobster', cursive;
   font-size: 84px;
   grid-area: createAccount;
-  display: inline;
+  /* display: inline; taking this our makes the text centered*/
 }
 
 label.sr-only-un {
@@ -174,16 +182,21 @@ label.sr-only-pw {
   grid-area: loginPass
 }
 
-input#username.form-control {
+#username.form-control {
+  display: block;
+  margin : 0 auto;
   font-family: 'Open Sans', sans-serif;
   resize: horizontal;
   width: 465px;
   height: 25px;
   text-align: center;
-  grid-area:usernameInput
+  grid-area:usernameInput;
+  border-radius: 10px;
 }
 
-.form-control {
+#password.form-control {
+  display: block;
+  margin : 0 auto;
   font-family: 'Open Sans', sans-serif;
   resize: vertical;
   width: 465px;
@@ -193,28 +206,66 @@ input#username.form-control {
   border-radius: 10px;
 }
 
+#confirmPassword.form-control {
+  display: block;
+  margin : 0 auto;
+  font-family: 'Open Sans', sans-serif;
+  resize: vertical;
+  width: 465px;
+  height: 25px;
+  text-align: center;
+  grid-area: passwordInput;
+  border-radius: 10px;
+}
+
+ input[type='radio'], label.brewlover, label.brewer{
+   text-align:justify; 
+   display:flex;
+   flex-direction:column;
+   align-items:center;
+   justify-content:center;
+   vertical-align: top;
+   width: 100%;
+ }
+
 label.brewlover{
+  width:100%;
+  margin:auto;
   font-family: 'Open Sans', sans-serif;
   grid-area: brewLover;
 }
 label.brewer{
+  margin: auto;
   font-family: 'Open Sans', sans-serif;
   grid-area: brewer;
 }
  button {
-  padding: 8px 32px;
-  font-family: 'Open Sans', sans-serif;
-  display: block;  
+  text-align:center; 
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  justify-content:center;
+  vertical-align: top;
+  width: 90%;
+  padding: 10px;
+  font-family: 'Open Sans', sans-serif;  
   grid-area: button;
+  
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
 } 
 
-input:active {
+/* input:active {
   width: auto;
 }
 
 input:focus {
   min-width: 200px
-}
+} */
 .body {
     margin: 0;
 }
