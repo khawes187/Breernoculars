b03724569eb6bdf102fdc724c4ea3bd789d9e4b9
Laura Brenner <laura.brenner@ymail.com>
2021-08-06 11:38:21 -0400

    working on brewery page, will fix after lunch

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,72 +1,6 @@
 <template class="template">
 <body>
-<!--   <div class="menuButtons">
-    <div id="home">
-      <router-link v-bind:to="{ name: 'home' }">
-        <button class="homeButton" style="border: 0; background: transparent">
-          <img src="@/images/editedbutton.gif" width="55" height="65" alt="Go Home" />
-          <div class="homeText">Home</div>
-        </button>
-      </router-link>
-    </div>
 
-    <div class="addBeerButton">
-      <button class="add" style="border: 0; background: transparent">
-          <img src="@/images/editedbutton.gif" width="55" height="65" alt="Add a beer" />
-          <div class="addBeerText">Add a Beer</div>
-      </button>
-    </div>
-    <div class="deleteBeerButton">
-      <button class="delete" style="border: 0; background: transparent">
-        <img src="@/images/editedbutton.gif" width="55" height="65" alt="Delete a beer" />
-        <div class="deleteBeerText">Delete a Beer</div>
-      </button>
-    </div> -->
-
-     <!--  needs linked to role//should this be a router-link? or v-if? -->
-<!--       <div class="addBeerFromBreweryButton">
-      <button class="add" style="border: 0; background: transparent">
-        <img src="@/images/editedbutton.gif" width="55" height="65" alt="Delete a beer" />
-        <div class="addBeerToBreweryText">Add Beer to Brewery</div>
-      </button>
-      </div>
-      <div class="deleteBeerFromBreweryButton">
-      <button class="delete" style="border: 0; background: transparent">
-        <img src="@/images/editedbutton.gif" width="55" height="65" alt="Delete a beer" />
-        <div class="deleteBeerFromBreweryText">Delete a Beer from Brewery</div>
-      </button>
-      </div> -->
-    
-
-    <!-- needs linked to role//should this be a router-link? or v-if? -->
-<!--     <div class="addBrewer">
-      <button class="add" style="border: 0; background: transparent">
-        <img src="@/images/editedbutton.gif" width="55" height="65" alt="Delete a beer" />
-        <div class="addBrewer">Add Brewer</div>
-      </button>
-      </div>
-    <div class="deleteBrewer">
-      <button class="delete" style="border: 0; background: transparent">
-        <img src="@/images/editedbutton.gif" width="55" height="65" alt="Delete a beer" />
-        <div class="deleteBrewer">Delete a Brewer</div> 
-      </button>
-    </div> -->
-
-    <!-- <div id="loginOrOut">
-      <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">
-        <button class="loginButton" style="border: 0; background: transparent">
-          <img src="@/images/editedbutton.gif" width="55" height="65" alt="Login" />
-          <div class="loginText">Login</div>
-        </button>
-      </router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">
-        <button class="logoutButton" style="border: 0; background: transparent">
-          <img src="@/images/editedbutton.gif" width="55" height="65" alt="logout" />
-          <div class="logoutText">Logout</div>
-        </button>
-      </router-link>
-    </div>
-  </div> -->
    <div class="home">
     <header>
       <h1>Working Title</h1>
@@ -115,39 +49,10 @@ export default {
 
 @import url('https://fonts.googleapis.com/css2?family=Lobster&family=Open+Sans&display=swap');
 
-/* .menuButtons{
-  height:100px;
-  width: 1000px; */
-  /* display: flex;
-  flex-direction: row;
-  justify-content:space-between; */
-  /* background: repeating-conic-gradient(lightblue 0% 25%, white 0% 50%) 50% / 20px 20px;
-  border-radius: 25px;
-  border-color: black;
-  border-style: groove;
-  display: grid;
-  grid-template-columns: auto auto auto auto;
-  grid-template-areas: 
-    "home add delete logInOrOut";
-  grid-area: menuBar;
-}
-.homebutton{
-  grid-area: home;
-}
-.add{
-  grid-area: add;
-}
-.delete{
-  grid-area: delete;
-}
-#loginOrOUt{
-  grid-area: logInOrOut;
-} */
 header {
   font-family: 'Lobster', cursive;
   grid-area: header;
   text-align: center;
-
 }
 h1 {
   color: goldenrod;
@@ -168,6 +73,7 @@ h1 {
   z-index: -1;
   }
  div.home {
+  cursor: pointer;
   display: grid;
   grid-template-columns: auto auto auto;
   grid-template-areas: 
