53895c1e3a3a38b6474e2eb762ee12a4bf39d4bd
Patrick Heil <theelectromagneticspectrum@gmail.com>
2021-08-10 21:15:11 -0400

    Untappd JSON data controller

---
+++ API/Capstone Server/Models/JSONResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone_Server.Models
+{
+    class JSONResponse
+    {
+        public Meta Meta { get; set; }
+        public Notifications Notifications { get; set; }
+        public Response Response { get; set; }
+
+    }
+}
---
+++ API/Capstone Server/Models/Meta.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone_Server.Models
+{
+    class Meta
+    {
+        public int code { get; set; }
+        public ResponseTime ResponseTime { get; set; }
+        public InitTime InitTime { get; set; }
+    }
+}
