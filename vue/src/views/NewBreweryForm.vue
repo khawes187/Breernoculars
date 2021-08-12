fd47ab7ecefab9383d268cfc7db583ab1d2954c0
Laura Brenner <laura.brenner@ymail.com>
2021-08-12 10:34:28 -0400

    made new brewery form pretty

--- vue/src/views/NewBreweryForm.vue
+++ vue/src/views/NewBreweryForm.vue
@@ -2,22 +2,22 @@
     <div id="create-brewery-form">
         <form v-on:submit.prevent="submitForm">
             <label for="breweryName">Brewery Name:</label><br>
-            <input type="text" id="breweryName" name="breweryName" v-model="brewery.breweryName"><br>
+            <input type="text" id="breweryName" name="breweryName" v-model="brewery.breweryName" size="40"><br>
             <br>
             <label for="breweryAddress">Brewery Address:</label><br>
-            <input type="text" id="breweryAddress" name="breweryAddress" v-model="brewery.breweryAddress"><br>
+            <textarea id="breweryAddress" name="breweryAddress" v-model="brewery.breweryAddress" rows="4" cols="40"></textarea><br>
             <br>
             <label for="breweryPhoneNumber">Brewery Phone Number:</label><br>
-            <input type="text" id="breweryPhoneNumber" name="breweryPhoneNumber" v-model="brewery.breweryPhoneNumber"><br>
+            <input type="text" id="breweryPhoneNumber" name="breweryPhoneNumber" v-model="brewery.breweryPhoneNumber" size="40"><br>
             <br>
             <label for="breweryWebsite">Brewery Website:</label><br>
-            <input type="text" id="breweryWebsite" name="breweryWebsite" v-model="brewery.breweryWebsite"><br>
+            <input type="text" id="breweryWebsite" name="breweryWebsite" v-model="brewery.breweryWebsite" size="40"><br>
             <br>
             <label for="breweryDateEstablished">Brewery Date Established:</label><br>
-            <input type="text" id="breweryDateEstablished" name="breweryDateEstablished" v-model="brewery.breweryDateEstablished"><br>
+            <input type="text" id="breweryDateEstablished" name="breweryDateEstablished" v-model="brewery.breweryDateEstablished" size="40"><br>
             <br>
             <label for="breweryHistory">Brewery History:</label><br>
-            <input type="text" id="breweryHistory" name="breweryHistory" v-model="brewery.breweryHistory"><br>
+            <textarea id="breweryHistory" name="breweryHistory" v-model="brewery.breweryHistory" rows="4" cols="40"></textarea><br>
             <br>
             <button type="submit" id="beer-submit-button" style="border: 0; background: transparent">
                 <img src="@/images/editedbutton.gif" width="55" height="65" alt="add brewery" />
@@ -77,16 +77,25 @@ export default {
 </script>     
 
 <style>
+body{
+    background-color: goldenrod;
+    
+}
 #create-brewery-form{
     background-color: goldenrod;
-    width: fit-content;
+    width: 100;
     padding: 10px;
     border-radius: 25px;
     text-align: center;
     margin-left: 10px;
     font-weight: bold;
+    font-size: larger;
 }
 .beer-submit-button{
     color: black;
+}
+.add-brewery-text{
+    font-size: larger;
+    font-weight: bold;
 }
 </style>
